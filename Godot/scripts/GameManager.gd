3f3b187c76b6e00183d1dd714bcd17c1a4a3318a
casonbobo <118225257+casonbobo@users.noreply.github.com>
2023-08-17 16:36:30 -0500

    Assests

--- Godot/scripts/GameManager.gd
+++ Godot/scripts/GameManager.gd
@@ -12,6 +12,6 @@ var game_paused : bool = false:
 		get_tree().paused = !game_paused
 		emit_signal("toggle_game_paused", game_paused)
 
-func _input(event):
+func _input(_event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		game_paused = !game_paused
