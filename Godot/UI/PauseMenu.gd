7582bb4ed3131ea50c3c0aa33de428b28978e935
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-08-23 02:41:22 -0500

    menu works! :D (mostly)
    - menu works when run the scene (need to make main scene btw)
    - pause menu can go to main menu
    - BUT when go to main menu from pause menu, it doesn't work?

--- Godot/UI/PauseMenu.gd
+++ Godot/UI/PauseMenu.gd
@@ -3,11 +3,16 @@ extends ColorRect
 @onready var animator: AnimationPlayer = $AnimationPlayer
 @onready var play_button: Button = find_child("Resume")
 @onready var quit_button: Button = find_child("Quit")
+@onready var menu_button: Button = find_child("Menu")
 
 func _ready() -> void:
 	play_button.pressed.connect(unpause)
 	quit_button.pressed.connect(get_tree().quit)
-	
+	menu_button.pressed.connect(menu)
+
+func menu():
+	get_tree().change_scene_to_file("res://nodes/Menu.tscn")
+
 func unpause():
 	animator.play("Unpause")
 	get_tree().paused = false
@@ -26,3 +31,4 @@ func _input(event):
 		else:
 			unpause()
 			print("unpause")
+
--- Godot/scripts/Menu.gd
+++ Godot/scripts/Menu.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var quit_button: Button = find_child("Quit")
+
+func _ready() -> void:
+	quit_button.pressed.connect(get_tree().quit)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Level1.tscn")
